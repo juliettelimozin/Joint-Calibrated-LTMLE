a5725bf01bf3758bd4ba741cc4f69710c79e0d9f
Juliette Limozin <juliette.limozin17@imperial.ac.uk>
2023-09-15 10:24:30 +0100

    HTML render of one trial calibration code

--- calibration_restrictions_draft_onetrial.R
+++ calibration_restrictions_draft_onetrial.R
@@ -133,81 +133,81 @@ bal.plot(eligible_data[eligible_data$t == 1,],stats = c('m', 'v'),var.name = c('
 
 
 
-PP <- TrialEmulation::data_modelling(data = switch_data,
-                                     outcome_cov = ~ X2 + X4+ assigned_treatment+
-                                       t_1 + t_2 + t_3 + t_4 +
-                                       t_1A + t_2A + t_3A + t_4A + 
-                                       t_1X2 + t_2X2 + t_3X2 + t_4X2 + 
-                                       t_1X4 + t_2X4 + t_3X4 + t_4X4,
-                                     model_var = c('assigned_treatment'),
-                                     glm_function = 'glm',
-                                     include_expansion_time = ~1, include_followup_time = ~1,
-                                     use_weight=1, use_censor=1, quiet = T, use_sample_weights =  F)
-switch_data$p_i <- predict.glm(PP$model, switch_data,type = 'response')
-
-switch_d0 <- readRDS(paste(data_direction,'/weight_model_switch_d0.rds', sep = ""))
-switch_n0 <- readRDS(paste(data_direction,'/weight_model_switch_n0.rds', sep = ""))
-switch_d1 <- readRDS(paste(data_direction,'/weight_model_switch_d1.rds', sep = ""))
-switch_n1 <- readRDS(paste(data_direction,'/weight_model_switch_n1.rds', sep = ""))
-
-design_mat <- expand.grid(id = 1:as.numeric(scenarios[l,1]),
-                          for_period = 0:4,
-                          followup_time = 0:4) 
-design_mat <- design_mat[which(5 -design_mat$for_period > design_mat$followup_time),]
-
-fitting_data_treatment <-  switch_data %>% 
-  dplyr::mutate(assigned_treatment = followup_time*0 + 1) %>% 
-  dplyr::select(id,for_period, followup_time, X2,  X4, assigned_treatment) %>% 
-  merge(design_mat, by = c("id", "for_period", "followup_time"), all.y = TRUE) %>% 
-  dplyr::group_by(id) %>% 
-  tidyr::fill( X2,X4,assigned_treatment,.direction = "down") %>% 
-  dplyr::ungroup() %>% 
-  dplyr::select(id, for_period, followup_time, X2, X4, assigned_treatment) %>% 
-  merge(data.frame(id = switch_data$id, for_period = switch_data$for_period), by = c("id", "for_period"), all.y = TRUE) %>% 
-  dplyr::arrange(id, for_period, followup_time) %>% 
-  dplyr::mutate(t_1 = ifelse(followup_time == 1,1,0),
-                t_2 = ifelse(followup_time == 2,1,0),
-                t_3 = ifelse(followup_time == 3,1,0),
-                t_4 = ifelse(followup_time == 4,1,0),
-                t_1A = t_1*assigned_treatment,
-                t_2A = t_2*assigned_treatment,
-                t_3A = t_3*assigned_treatment,
-                t_4A = t_4*assigned_treatment,
-                t_1X2 = t_1*X2,
-                t_2X2 = t_2*X2,
-                t_3X2 = t_3*X2,
-                t_4X2 = t_4*X2,
-                t_1X4 = t_1*X4,
-                t_2X4 = t_2*X4,
-                t_3X4 = t_3*X4,
-                t_4X4 = t_4*X4) %>% 
-  dplyr::filter(for_period == 0)
-
-fitting_data_treatment <- fitting_data_treatment[!duplicated(fitting_data_treatment),]
-
-fitting_data_control <- fitting_data_treatment %>% 
-  dplyr::mutate(assigned_treatment = assigned_treatment*0,
-                t_1A = t_1*0,
-                t_2A = t_2*0,
-                t_3A = t_3*0,
-                t_4A = t_4*0)
-
-Y_pred_PP_treatment <- predict.glm(PP$model, 
-                                   fitting_data_treatment, 
-                                   type = "response")
-Y_pred_PP_control <- predict.glm(PP$model, 
-                                 fitting_data_control,
-                                 type = "response")
-predicted_probas_PP <- fitting_data_treatment %>% 
-  dplyr::mutate(predicted_proba_treatment = Y_pred_PP_treatment,
-                predicted_proba_control = Y_pred_PP_control) %>% 
-  dplyr::group_by(id, for_period) %>% 
-  dplyr::mutate(cum_hazard_treatment = cumprod(1-predicted_proba_treatment),
-                cum_hazard_control = cumprod(1-predicted_proba_control)) %>% 
-  dplyr::ungroup() %>% 
-  dplyr::group_by(followup_time) %>% 
-  dplyr::summarise(survival_treatment = mean(cum_hazard_treatment),
-                   survival_control = mean(cum_hazard_control),
-                   survival_difference = survival_treatment - survival_control)
+# PP <- TrialEmulation::data_modelling(data = switch_data,
+#                                      outcome_cov = ~ X2 + X4+ assigned_treatment+
+#                                        t_1 + t_2 + t_3 + t_4 +
+#                                        t_1A + t_2A + t_3A + t_4A + 
+#                                        t_1X2 + t_2X2 + t_3X2 + t_4X2 + 
+#                                        t_1X4 + t_2X4 + t_3X4 + t_4X4,
+#                                      model_var = c('assigned_treatment'),
+#                                      glm_function = 'glm',
+#                                      include_expansion_time = ~1, include_followup_time = ~1,
+#                                      use_weight=1, use_censor=1, quiet = T, use_sample_weights =  F)
+# switch_data$p_i <- predict.glm(PP$model, switch_data,type = 'response')
+# 
+# switch_d0 <- readRDS(paste(data_direction,'/weight_model_switch_d0.rds', sep = ""))
+# switch_n0 <- readRDS(paste(data_direction,'/weight_model_switch_n0.rds', sep = ""))
+# switch_d1 <- readRDS(paste(data_direction,'/weight_model_switch_d1.rds', sep = ""))
+# switch_n1 <- readRDS(paste(data_direction,'/weight_model_switch_n1.rds', sep = ""))
+# 
+# design_mat <- expand.grid(id = 1:as.numeric(scenarios[l,1]),
+#                           for_period = 0:4,
+#                           followup_time = 0:4) 
+# design_mat <- design_mat[which(5 -design_mat$for_period > design_mat$followup_time),]
+# 
+# fitting_data_treatment <-  switch_data %>% 
+#   dplyr::mutate(assigned_treatment = followup_time*0 + 1) %>% 
+#   dplyr::select(id,for_period, followup_time, X2,  X4, assigned_treatment) %>% 
+#   merge(design_mat, by = c("id", "for_period", "followup_time"), all.y = TRUE) %>% 
+#   dplyr::group_by(id) %>% 
+#   tidyr::fill( X2,X4,assigned_treatment,.direction = "down") %>% 
+#   dplyr::ungroup() %>% 
+#   dplyr::select(id, for_period, followup_time, X2, X4, assigned_treatment) %>% 
+#   merge(data.frame(id = switch_data$id, for_period = switch_data$for_period), by = c("id", "for_period"), all.y = TRUE) %>% 
+#   dplyr::arrange(id, for_period, followup_time) %>% 
+#   dplyr::mutate(t_1 = ifelse(followup_time == 1,1,0),
+#                 t_2 = ifelse(followup_time == 2,1,0),
+#                 t_3 = ifelse(followup_time == 3,1,0),
+#                 t_4 = ifelse(followup_time == 4,1,0),
+#                 t_1A = t_1*assigned_treatment,
+#                 t_2A = t_2*assigned_treatment,
+#                 t_3A = t_3*assigned_treatment,
+#                 t_4A = t_4*assigned_treatment,
+#                 t_1X2 = t_1*X2,
+#                 t_2X2 = t_2*X2,
+#                 t_3X2 = t_3*X2,
+#                 t_4X2 = t_4*X2,
+#                 t_1X4 = t_1*X4,
+#                 t_2X4 = t_2*X4,
+#                 t_3X4 = t_3*X4,
+#                 t_4X4 = t_4*X4) %>% 
+#   dplyr::filter(for_period == 0)
+# 
+# fitting_data_treatment <- fitting_data_treatment[!duplicated(fitting_data_treatment),]
+# 
+# fitting_data_control <- fitting_data_treatment %>% 
+#   dplyr::mutate(assigned_treatment = assigned_treatment*0,
+#                 t_1A = t_1*0,
+#                 t_2A = t_2*0,
+#                 t_3A = t_3*0,
+#                 t_4A = t_4*0)
+# 
+# Y_pred_PP_treatment <- predict.glm(PP$model, 
+#                                    fitting_data_treatment, 
+#                                    type = "response")
+# Y_pred_PP_control <- predict.glm(PP$model, 
+#                                  fitting_data_control,
+#                                  type = "response")
+# predicted_probas_PP <- fitting_data_treatment %>% 
+#   dplyr::mutate(predicted_proba_treatment = Y_pred_PP_treatment,
+#                 predicted_proba_control = Y_pred_PP_control) %>% 
+#   dplyr::group_by(id, for_period) %>% 
+#   dplyr::mutate(cum_hazard_treatment = cumprod(1-predicted_proba_treatment),
+#                 cum_hazard_control = cumprod(1-predicted_proba_control)) %>% 
+#   dplyr::ungroup() %>% 
+#   dplyr::group_by(followup_time) %>% 
+#   dplyr::summarise(survival_treatment = mean(cum_hazard_treatment),
+#                    survival_control = mean(cum_hazard_control),
+#                    survival_difference = survival_treatment - survival_control)
 
 estimates[,i] <- pull(predicted_probas_PP,survival_difference)
