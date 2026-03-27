6cee5113f048a441e87386ecdcd9c69add460ec4
juliettelimozin <juliette.limozin@gmail.com>
2026-03-27 13:54:24 +0000

    MLE CI computation time

--- results_ci_coverage_simu.R
+++ results_ci_coverage_simu.R
@@ -29,6 +29,7 @@ for(i in c(300,500,1000,2500)){
   colnames(coverage) <- c('Modified boot.', 'Modified boot. with cali', 'Normal boot.')
   
   ci_coverage_simu_result <- readRDS(paste0("ci_coverage_simu_result_",i,".rds"))
+  mle_comp_result <- readRDS(paste0("mle_ci_comp_time_weak_conf_result_",i,".rds"))
   
   thr <- c(200,200,200,10,10,10,230,230,230)
   indLB <- sweep(ci_coverage_simu_result$modified_bootstrap_CIs_LB, 1, thr, FUN = "<=")
@@ -60,6 +61,8 @@ for(i in c(300,500,1000,2500)){
   width[,3] <- rowMeans(ci_coverage_simu_result$normal_bootstrap_CIs_UB - ci_coverage_simu_result$normal_bootstrap_CIs_LB)
   
   computation_time <- rowMeans(ci_coverage_simu_result$computation_time)
+  mle_computation_time <- rowMeans(mle_comp_result$computation_time)
+  
   names(computation_time) <- c('Modified boot.', 'Modified boot. with cali', 'Normal boot.')
   
   coverage <- t(coverage)
@@ -75,7 +78,7 @@ for(i in c(300,500,1000,2500)){
                              'MLE LTMLE beta_1')],
                  width[c(3,1,2),c('CMLE LTMLE beta_0',
                              'CMLE LTMLE beta_1')])
-  computation_time <- c(NA,NA,computation_time[c(3,1,2)])
+  computation_time <- c(mle_computation_time[c(2,1)],computation_time[c(3,1,2)])
   
   results_table <- cbind(coverage,width, computation_time)
   
@@ -94,6 +97,7 @@ for(i in c(300,500,1000,2500)){
   colnames(coverage) <- c('Modified boot.', 'Modified boot. with cali', 'Normal boot.')
   
   ci_coverage_simu_result <- readRDS(paste0("ci_coverage_simu_strong_conf_result_",i,".rds"))
+  mle_comp_result <- readRDS(paste0("mle_ci_comp_time_strong_conf_result_",i,".rds"))
   
   thr <- c(200,200,200,10,10,10,230,230,230)
   indLB <- sweep(ci_coverage_simu_result$modified_bootstrap_CIs_LB, 1, thr, FUN = "<=")
@@ -125,6 +129,8 @@ for(i in c(300,500,1000,2500)){
   width[,3] <- rowMeans(ci_coverage_simu_result$normal_bootstrap_CIs_UB - ci_coverage_simu_result$normal_bootstrap_CIs_LB)
   
   computation_time <- rowMeans(ci_coverage_simu_result$computation_time)
+  mle_computation_time <- rowMeans(mle_comp_result$computation_time)
+  
   names(computation_time) <- c('Modified boot.', 'Modified boot. with cali', 'Normal boot.')
   
   coverage <- t(coverage)
@@ -140,7 +146,8 @@ for(i in c(300,500,1000,2500)){
                                 'MLE LTMLE beta_1')],
                  width[c(3,1,2),c('CMLE LTMLE beta_0',
                           'CMLE LTMLE beta_1')])
-  computation_time <- c(NA,NA,computation_time[c(3,1,2)])
+  computation_time <- c(mle_computation_time[c(2,1)],computation_time[c(3,1,2)])
+  
   results_table <- cbind(results_table, cbind(coverage,width, computation_time))
   
   rownames(results_table)<- c('MLE Normal.boot.','MLE Modified.boot.', 'CMLE Normal boot.', 'CMLE Modified boot.', 'CMLE Modified boot. with cali')
